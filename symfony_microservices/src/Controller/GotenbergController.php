2d58b6a7a5c14d81a2b0c66a9c7438657e4e11a4
Camélien <camelien.tournu@etudiant.univ-reims.fr>
2024-04-18 10:10:35 +0200

    feature: add controllers/service/route

---
+++ symfony_microservices/src/Controller/GotenbergController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\GotenbergService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GotenbergController extends AbstractController
+{
+    private $gotenbergService;
+
+    public function __construct(GotenbergService $gotenbergService)
+    {
+        $this->gotenbergService = $gotenbergService;
+    }
+
+    #[Route("/generate-pdf", name: "generate_pdf", methods: ["POST"])]
+
+    public function generatePdf(Request $request): Response
+    {
+        $htmlContent = $request->getContent();
+
+        $pdfContent = $this->gotenbergService->generatePdf($htmlContent);
+
+        return new Response($pdfContent, 200, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'attachment; filename="generated.pdf"',
+        ]);
+    }
+}
